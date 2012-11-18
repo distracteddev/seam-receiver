23da17644bf29f09cab32e234cfa03bf3e492d76
Zeus <lalkaka.zeus@gmail.com>
2012-11-17 23:46:09 -0800

    tests

--- test/master.js
+++ test/master.js
@@ -13,3 +13,50 @@ master.listen(function(err) {
     throw err;
   }
 });
+
+delay = setTimeout(function() {
+  console.log(exports);
+}, 1000);
+
+
+exports.simulateDeploy = function () {
+  var notif = {};
+  notif.name = 'Soapbox';
+  notif.type = 'deploy/pass';
+  notif.message  = 'Soapbox Successfully Deployed';
+  notif.url      = 'zeus.ly';
+  master.emit('notification', notif);
+};
+
+exports.simulateDeployFailure = function () {
+  var notif = {};
+  notif.name = 'Soapbox';
+  notif.type = 'deploy/fail';
+  notif.message  = 'Soapbox Failed to Deploy';
+  master.emit('notification', notif);
+};
+
+exports.simulateAppError = function () {
+  var notif = {};
+  notif.name = 'Soapbox';
+  notif.type = 'deploy/fail';
+  notif.message  = 'Error: TypeError Blah Blah Blah';
+  master.emit('notification', notif);
+};
+
+exports.simulateInfo = function () {
+  var notif = {};
+  notif.name = 'Soapbox';
+  notif.type = 'deploy/fail';
+  notif.message  = 'Soapbox Failed to Deploy';
+  master.emit('notification', notif);
+};
+
+exports.simulateTestsPassed = function () {
+
+};
+
+exports.simulateTestsFailed = function () {
+
+
+};
